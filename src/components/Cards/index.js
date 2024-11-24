8819c09186215823055ff7d5aab049d26b476b4b
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-24 10:44:20 +0530

    Modal created

---
+++ src/components/Cards/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card, Row } from "antd";
+import "./styles.css"
+import Button from "../Button";
+function Cards({
+  currentBalance,
+  income,
+  expenses,
+  showExpenseModal,
+  showIncomeModal,
+  reset,
+}) {
+  return (
+    <Row 
+    className="my-row"
+    >
+      <Card bordered={true} className="my-card">
+        <h2>Current Balance</h2>
+        <p>₹{currentBalance}</p>
+        <Button blue text="Reset"/>
+
+      </Card>
+
+      <Card bordered={true} className="my-card">
+        <h2>Total Income</h2>
+        <p>₹{income}</p>
+        <Button blue text="Add Income" onClick={showIncomeModal}/>
+
+      </Card>
+
+      <Card bordered={true} className="my-card">
+        <h2>Total Expenses</h2>
+        <p>₹{expenses}</p>
+        <Button blue text="Add Expense" onClick={showExpenseModal}/>
+
+      </Card>
+    </Row>
+  );
+}
+
+export default Cards;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Cards from "../components/Cards";
+import { Modal } from "antd";
+import AddExpenseModal from "../components/Modals/addExpense";
+import AddIncomeModal from "../components/Modals/addIncome";
 function Dashboard() {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  };
+
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  };
+
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  };
+
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
+
+  const onFinish = (values,type ) => {
+    console.log("He",{values,type});
+  };
+
   return (
     <div>
-      <Header/>
-      Dashboard
+      <Header />
+      <Cards
+        showExpenseModal={showExpenseModal}
+        showIncomeModal={showIncomeModal}
+      />
+      <AddExpenseModal
+        isExpenseModalVisible={isExpenseModalVisible}
+        handleExpenseCancel={handleExpenseCancel}
+        onFinish={onFinish}
+      />
+      <AddIncomeModal
+        isIncomeModalVisible={isIncomeModalVisible}
+        handleIncomeCancel={handleIncomeCancel}
+        onFinish={onFinish}
+      />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
